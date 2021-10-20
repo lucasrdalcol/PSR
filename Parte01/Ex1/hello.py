2c2f7ceb3a6b47e66b74c1376e1d5030a19fcf2b
lucasrdalcol <lucasrdalcol@ua.pt>
2021-10-20 19:33:52 +0100

    Adding exercises of Parte01.

---
+++ Parte01/Ex1/hello.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3.8
+
+def main():
+
+    students = ['Lucas', 'Miguel', 'Manuel']
+    for i, student in enumerate(students):
+        print('Hello ' + str(i) + ' which name is ' + student)
+
+if __name__ == "__main__":
+    main()

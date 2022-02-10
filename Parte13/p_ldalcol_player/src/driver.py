68a64e21ad7a878b496b37e1b1b25bacca53f142
lucasrdalcol <lucasrdalcol@ua.pt>
2022-02-10 20:03:48 +0000

    parte13 done.

--- Parte13/p_ldalcol_player/src/driver.py
+++ Parte13/p_ldalcol_player/src/driver.py
@@ -6,55 +6,85 @@
 import argparse
 import copy
 import math
-
 import colorama
 import rospy
 import tf2_ros
 from std_msgs.msg import String
 from psr_parte09_exs.msg import Dog
-from geometry_msgs.msg import Twist, PoseStamped
+from geometry_msgs.msg import Twist
+from tf2_geometry_msgs import PoseStamped
 
 
-class Driver():
+class Driver:
     def __init__(self):
-        self.publisher_command = rospy.Publisher('p_ldalcol/cmd_vel', Twist, queue_size=1)
-
-        self.tf_buffer = tf2_ros.Buffer()
-        self.listener = tf2_ros.TransformListener(self.tf_buffer)
-
-        self.timer = rospy.Timer(rospy.Duration(0.1), self.sendCommandCallback)
-
-        self.goal_subscriber = rospy.Subscriber('/move_base_simple/goal', PoseStamped,
-                                                self.goalReceivedCallback)  # Subscribe the node to the specified topic
-
+        # Define all variables
+        self.name = rospy.get_name()
+        self.name = self.name.strip('/')  # remove initial /
+        print('player name is ' + self.name)
         self.goal = PoseStamped()
         self.goal_active = False
-
         self.angle = 0
         self.speed = 0
 
-    def goalReceivedCallback(self, msg):
-        print('Received new goal')
-        frame_id = msg.header.frame_id
-        if 'odom' in frame_id:
-            self.goal = copy.deepcopy(msg)
-            self.goal_active = True
-        else:
-            rospy.logwarn('move_base_simple/goal is not in odom frame_id, aborting!!!')
-            exit(0)
+        # Initialize publisher
+        self.publisher_command = rospy.Publisher('/' + self.name + '/cmd_vel', Twist, queue_size=1)
 
-    def driveStraight(self):
+        # Initialize tf2 buffer and listener
+        self.tf_buffer = tf2_ros.Buffer()
+        self.listener = tf2_ros.TransformListener(self.tf_buffer)
+
+        # Use Timer to set a Command Callback
+        self.timer = rospy.Timer(rospy.Duration(0.1), self.sendCommandCallback)
+
+        # Initialize the subscriber to receive the goal pose
+        self.goal_subscriber = rospy.Subscriber('/move_base_simple/goal', PoseStamped,
+                                                self.goalReceivedCallback)  # Subscribe the node to the specified topic
+
+    def goalReceivedCallback(self, msg):
+        """
+
+        :param msg:
+        """
+        print('Received new goal')
+        target_frame = self.name + '/odom'
+
+        try:
+            self.goal = self.tf_buffer.transform(msg, target_frame, rospy.Duration(1))
+            self.goal_active = True
+            rospy.loginfo('Setting new goal on frame ' + target_frame)
+        except (tf2_ros.LookupException, tf2_ros.ConnectivityException, tf2_ros.ExtrapolationException):
+            self.goal_active = False
+            rospy.logerr('Could not transform goal from ' + msg.header.frame_id + ' to ' + target_frame +
+                         '. Ignoring this goal.')
+
+    def driveStraight(self, minimum_speed=0.1, maximum_speed=0.75):
         goal_copy = copy.deepcopy(self.goal)
         goal_copy.header.stamp = rospy.Time.now()
-        goal_in_base_link = self.tf_buffer.transform(goal_copy, 'p_ldalcol/base_footprint', rospy.Duration(1))
+        goal_in_base_link = self.tf_buffer.transform(goal_copy, self.name + '/base_footprint', rospy.Duration(1))
 
-        self.angle = math.atan2(goal_in_base_link.pose.position.y, goal_in_base_link.pose.position.x)
-        self.speed = 0.1  # TODO vary speed according to distance to goal
+        x = goal_in_base_link.pose.position.x
+        y = goal_in_base_link.pose.position.y
+
+        self.angle = math.atan2(y, x)
+
+        print(self.angle)
+        distance_to_goal = math.sqrt(x ** 2 + y ** 2)
+
+        self.speed = max(minimum_speed, 0.5 * distance_to_goal)  # limit minimum speed
+        self.speed = min(maximum_speed, self.speed)  # limit maximum speed
+
+        if distance_to_goal < 0.08:
+            self.speed = 0
 
     def sendCommandCallback(self, event):
         print('Sending twist command')
 
-        self.driveStraight()
+        if not self.goal_active:  # No goal, no movement
+            self.speed = 0
+            self.angle = 0
+        else:
+            self.driveStraight()
+
         twist = Twist()
         twist.linear.x = self.speed
         twist.angular.z = self.angle
